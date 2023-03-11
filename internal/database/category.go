905ec0587fdf328964b73ef2306b9523687aa748
Jorlano D S Pereira <souojor@gmail.com>
2023-03-11 16:56:45 -0300

    new services

--- internal/database/category.go
+++ internal/database/category.go
@@ -44,6 +44,16 @@ func (c *Category) FindAll() ([]Category, error) {
 	return categories, nil
 }
 
+func (c *Category) Find(categoryID string) (Category, error) {
+	var id, name, description string
+	err := c.db.QueryRow("SELECT id, name, description FROM categories WHERE id = $1", categoryID).
+		Scan(&id, &name, &description)
+	if err != nil {
+		return Category{}, err
+	}
+	return Category{ID: id, Name: name, Description: description}, nil
+}
+
 func (c *Category) FindByCourseID(courseID string) (Category, error) {
 	var id, name, description string
 	err := c.db.QueryRow("SELECT c.id, c.name, c.description FROM categories c JOIN courses co ON c.id = co.category_id WHERE co.id = $1", courseID).
